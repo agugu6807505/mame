64d9b81a3367238086b3c3585b227063fea05450
agugu2000 <6807505@qq.com>
2024-09-03 14:05:22 +0800

    cv1k在编译街机版时的修正

--- scripts/src/video.lua
+++ scripts/src/video.lua
@@ -1684,3 +1684,21 @@ if (VIDEOS["ZR36110"]~=null) then
 		MAME_DIR .. "src/devices/video/zr36110.h",
 	}
 end
+
+if _OPTIONS["subtarget"]=="arcade" then
+	files {
+		MAME_DIR .. "src/mame/cave/ep1c12.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12.h",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit0.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit1.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit2.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit3.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit4.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit5.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit6.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit7.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12_blit8.cpp",
+		MAME_DIR .. "src/mame/cave/ep1c12in.ipp",
+		MAME_DIR .. "src/mame/cave/ep1c12pixel.ipp",
+	}
+end
